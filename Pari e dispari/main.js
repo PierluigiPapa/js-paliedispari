6729b0b4bdf77b8948b5d099d95d6370c32f86e7
PierluigiPapa <pierluigipapa9@gmail.com>
2024-01-15 18:20:03 +0100

    feat: add sum's function

--- Pari e dispari/main.js
+++ Pari e dispari/main.js
@@ -1,15 +1,33 @@
 // Varibili per chiedere all'utente di scegliere pari o dispari e inserire un numero da 1 a 5
 let pariDispari = prompt("Scegli un pari o dispari:");
 let numeroUtente = parseInt(prompt("Inserisci un numero da 1 a 5:"));
-// console.log (sceltaUtente, numeroUtente)
+
+//Variabile per generare il numero casuale
+let numeroPc = numeroRandom();
+
+//Variabile per determinare il risultato
+let risultato = numeroRisultato;
+
+if (risultato === pariDispari) {
+    console.log("Hai vinto")
+} else{
+    console.log("Hai perso");
+}
 
 //Funzione per generare un numero random da 1 a 5
 function numeroRandom (){
     return Math.floor(Math.random()* 5) + 1;
 }
 
-//Costante per generare il numero random del computer
-let numeroPc = numeroRandom()
-
-//Variabile per sommare i due numeri (utente e pc)
-let somma = numeroUtente + numeroPc
+// Funzione per stabilire la somma
+function numeroRisultato() {
+    let somma = numeroPc + numeroUtente;
+    console.log("La somma dei due numeri è: "+ somma);
+    //SE la somma è pari
+    if (somma % 2 === 0){
+        return "pari"
+    //ALTRIMENTI la somma è dispari
+    } else{
+        return "dispari"
+    }
+}
