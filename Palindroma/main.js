3765803153dd5671efe6a52e3a3de982d275977d
PierluigiPapa <pierluigipapa9@gmail.com>
2024-01-15 16:21:29 +0100

    feat: add const in Palindroma

--- Palindroma/main.js
+++ Palindroma/main.js
@@ -1,10 +1,25 @@
+//Costante per creare il prompt dove l'utente inserisce la parola
+const parolaMinuscola = prompt ("Inserisci una parola");
+
 //Funzione per controllare la parola
 function checkParolaPalindroma (parola) {
     if(parola.lenght === 0) {
         return false;
     }
 
-    if (parola.lenght === 1) {
+    //Costante per inserire la parola tutta minuscola
+    const parolaMinuscola = parola.toLowerCase();
+
+    //Variabile per inserire la parola intera
+    let inizio = 0;
+    //Variabile per inserire la parola invertita
+    let fine = parola.lenght -1;
+
+    while (inizio < fine) {
+        if (parolaMinuscola[inizio] !== parolaMinuscola[fine]) {
+            return false; }
+            inizio++;
+            fine--;
+        }
         return true;
-    }
 }
