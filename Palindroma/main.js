25a496b1821df4a777da6918d38f7c915837b27c
PierluigiPapa <pierluigipapa9@gmail.com>
2024-01-15 15:53:19 +0100

    feat: add function

---
+++ Palindroma/main.js
@@ -0,0 +1,10 @@
+//Funzione per controllare la parola
+function checkParolaPalindroma (parola) {
+    if(parola.lenght === 0) {
+        return false;
+    }
+
+    if (parola.lenght === 1) {
+        return true;
+    }
+}
